6f525af4893a9dfe805304a8eaa746d985bba60b
Tobias Saaby Steffensen <tobias.saaby@gmail.com>
2023-03-20 16:30:56 +0000

    Flag for SQLi CTF

--- CTFs/SQLi/welcome.php
+++ CTFs/SQLi/welcome.php
@@ -22,6 +22,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <div class="page-header">
         <h1>Welcome <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. </h1>
+        <p>you have successfully logged in.</p>
+        <?php
+            if(empty($_SESSION["id"])){
+                echo "FLAG-9c4e69e4cfd239d4e16ba0c189f8d643";
+            }
+        ?>
     </div>
     <p>
         <a href="logout.php" class="btn btn-danger">logout</a>
